0d692ae4720a8721ac3b1c755c93f22869d72f52
Djamal <alidoumadougoudjamal@gmail.com>
2025-06-16 14:50:57 +0100

    modification des fichiers js

--- authentificate_user/static/accueil.js
+++
@@ -1,5 +0,0 @@
-
-function toggleMenu() {
-      document.getElementById('mobile-menu').classList.toggle('hidden');
-}
-
--- chat/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import MessageListCreateAPIViews
-
-urlpatterns = [
-    path('messages/', MessageListCreateAPIViews.as_view(), name='message-list-create'),
-]
