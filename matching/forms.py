ddbbf8723f60e3b7b3af1435eafc440d885bcb6b
EudesHDN <hodonoueudeshdn@gmail.com>
2025-06-16 09:18:48 -0700

    Modification du fichiers forms

--- matching/forms.py
+++ matching/forms.py
@@ -2,14 +2,20 @@ from django.utils import timezone
 from django import forms
 from .models import Offre,Demande
 from datetime import date
-from .models import max_length
 
 class SomeForm(forms.ModelForm):
     class Meta:
         model = Offre
         fields = ["driver","departTime","nbPLacesDispo"]
         widgets = {
-            "departPoint":{}
+            'departPoint': {
+                'departTime': forms.TextInput(attrs={"disabled":True,"title":"Point de départ"}),
+                'endTime': forms.TextInput(attrs={"disabled":True,"title":"Point de départ"})
+            }
+        }
+        labels = {
+            "departTime":"",
+            "endTime":""
         }
     def cleanTask(self):
         deadline = self.cleaned_data["deadline"]
