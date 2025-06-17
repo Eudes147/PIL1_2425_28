b843e2c6458431f9768746e5120b17f6cd9ac6d3
KOUASSI-Ken <kouassiken51@gmail.com>
2025-06-17 07:59:17 +0100

    urls.py

--- matching/urls.py
+++ matching/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
+
 from .views import matchingPas_Drv
 from .views import accueil_secondaire
 
 urlpatterns = [
     path("matching/<int:demande_id>/",matchingPas_Drv,name="matching"),
     path('accueil_secondaire/', accueil_secondaire, name='accueil_secondaire'),
-=======
+
+
 import views
 
 urlpatterns = [
@@ -16,5 +17,5 @@ urlpatterns = [
     path("createDemande <int:passenger_id>",views.createDemande,name="demande"),
     path("choicePosition <int:user_id>",views.choicePosition,name="takePosition"),
     path("waiting/matching <int:user_id>",views.waiting,name="waiting")
->>>>>>> d73ba9b5f56f681a64c4a537b2a4ecb75beebaae
+
 ]
